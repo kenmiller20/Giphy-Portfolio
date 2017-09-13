6fce890ecf7b65a090335494bc08d49adffb42d7
kenmiller20 <kenmiller20@yahoo.com>
2017-09-13 15:54:44 -0400

    Backup due to Windows crash

---
+++ assets/javascript/app.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+
+	// Initial array of villains
+	var villains = ['Loki', 'Harley Quinn', 'Thanos', 'The Joker'];
+
+	// Function for displaying villain data 
+	function renderButtons() {
+
+		// Deletes the movies prior to adding new animals (this is necessary otherwise you will have repeat buttons)
+		$('#superButtons').empty();
+
+		// Loops through the array of movies
+		for (var i = 0; i < villains.length; i++) {
+
+			// Dynamicaly generates buttons for each villain in the array
+			var still = villains[i].images.fixed_height.url;
+			var animate = villains[i].images.fixed_height.url;
+			var a = $('<button>'); // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+			
+			a.addClass('villain'); // Added a class 
+			a.attr('data-name', villains[i]); // Added a data-attribute
+			a.attr('src', $(this).data('animate'));
+			a.attr('data-state', $(this).attr('data-state', 'animate'));
+			a.text(villains[i]); // Provided the initial button text
+			$('#superButtons').append(a); // Added the button to the HTML
+		}
+	}
+
+	// This function handles events where one button is clicked
+	$('#addButton').on('click', function () {
+
+		// This line of code will grab the input from the textbox
+		var superv = $('#super-input').val().trim();
+
+		// The villain from the textbox is then added to our array
+		villains.push(superv);
+
+		// Our array then runs which handles the processing of our villain array
+		renderButtons();
+
+		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+		return false;
+	});
+
+	// This calls the renderButtons() function
+	renderButtons();
+
+	// The next section performs the search and returns the GIFs
+	$(document).on('click', '.villain', function () {
+	
+		var superv = $(this).data('name');
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + superv + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+		$.ajax({
+			url: queryURL,
+			method: 'GET'
+		})
+		.done(function (response) {
+
+			console.log(queryURL);
+			console.log(response);
+
+			var results = response.data;
+
+			$('#supervillain').empty();
+
+			for (var i = 0; i < results.length; i++) {
+
+				var villainDiv = $('<div style = "display: inline-block">');
+
+				var p = $('<p>').text("Rating: " + results[i].rating);
+
+				var villainImage = $('<img>');
+				villainImage.attr('src', results[i].images.fixed_height_still.url);
+
+				villainDiv.prepend(p);
+				villainDiv.prepend(villainImage);
+
+				$('#supervillain').prepend(villainDiv);
+
+			}
+			// The next section should allow for pausing and unpausing of the GIFs
+		});
+	});
+
+	$("#supervillain").on('click', '.vimg', function () {
+
+		var state = $(this).attr('data-state');
+
+	 		if ( state == 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+	});
+});   
--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,1 +1,120 @@
+var giphyPortfolio = {
+
+	topic: ['Pointer Dog', 'Puppy', 'Cat', 'Ragdoll Cat', 'Kitten', 'Hamster', 'Gerbil', 'Goldfish', 'Ferret'],
+
+
+	initButtons: function() {
+		// Clear existing buttons
+		$("#animal-topics").empty();
+
+		this.topic.forEach(this.addButton);
+	},
+
+	addButton: function(item, index) {
+        var $input = $('<button class="btn btn-primary btn-animal" value="'+item+'"><h4>'+item+'</h4></button>');
+
+        $input.appendTo($("#animal-topics"));	  
+	},
+};
+
+
+giphyPortfolio.initButtons();
+
+
+// When page is loaded
+$(document).ready(function() {
+
+	
+ //   $(".gif").on("click", function() {
+   $(document).on("click", ".gif", function() { 	
+    	console.log(this);
+
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+
+      console.log(state);
+
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
+
+	
+	$("#run-search").on("click", function(event) {
+	  	// This line allows us to take advantage of the HTML "submit" property
+	  	// This way we can hit enter on the keyboard and it registers the search
+	  	// (in addition to clicks).
+	  	event.preventDefault();
+
+	  	// This line of code will grab the input from the textbox
+		var animal = $('#added-animal').val().trim();
+
+		// Add animal to topic array
+		giphyPortfolio.topic.push(animal.replace(/\b\w/g, l => l.toUpperCase()));
+
+		// Redisplay buttons
+		giphyPortfolio.initButtons();
+
+		$('#added-animal').val("");
+	});
+
+
+	// 
+	$(document).on("click", ".btn-animal", function() {
+//	$(".btn-animal").on("click", function() {		
+		console.log(this.value);
+
+	  	$.ajax ({
+	    	url: "https://api.giphy.com/v1/gifs/search?q="+this.value+"&api_key=dc6zaTOxFJmzC&limit=10",
+	    	type: "GET",
+
+	    success: function(response) {
+	      console.log(response.data);
+
+			// Clear existing images
+			$("#animal-images").empty();
+
+			var results = response.data;
+
+	      	for (var idx=0; idx<response.data.length; idx++) {
+
+		      	// Saving the still and animated images
+		        var animiatedImageUrl = results[idx].images.fixed_height.url;
+		        var stillImageUrl = results[idx].images.fixed_height_still.url;
+
+		    	// Creating a paragraph tag with the result item's rating
+		        var p = $('<p>').text("Rating: " + results[idx].rating);
+
+				// Creating and storing an image tag
+		    	var animalImage = $("<img>");
+
+		        // Setting the animalImage src attribute to imageUrl
+		        animalImage.attr("src", stillImageUrl);
+		        animalImage.attr("alt", this.value+" image");
+
+		        animalImage.attr("data-state", "still");
+		        animalImage.attr("data-animate", animiatedImageUrl);
+		        animalImage.attr("data-still", stillImageUrl);
+		        animalImage.attr("class", "gif");
+
+console.log(animalImage);
+
+		        // Prepending the catImage to the images div
+		        $("#animal-images").append(animalImage);
+		       	// $("#animal-images").append(p);
+	     	}
+	    }, 
+
+	    error: function(e) {console.log("uh oh"); }
+	    });
+	});	
+});
+
 

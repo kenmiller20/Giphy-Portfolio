21a534c9f4af7a273c3b4c81b0c9bde51f290372
kenmiller20 <kenmiller20@yahoo.com>
2017-09-15 22:20:16 -0400

    Final Submission

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,114 +1,118 @@
 var giphyPortfolio = {
 
+	// Existing animal topics
 	topic: ['Pointer Dog', 'Puppy', 'Cat', 'Ragdoll Cat', 'Kitten', 'Hamster', 'Gerbil', 'Goldfish', 'Ferret'],
 
-
+	// Create one button for each topic in array
 	initButtons: function() {
 		// Clear existing buttons
 		$("#animal-topics").empty();
 
+		// Loop though topics
 		this.topic.forEach(this.addButton);
 	},
 
+	// Add topic button
 	addButton: function(item, index) {
         var $input = $('<button class="btn btn-primary btn-animal" value="'+item+'"><h4>'+item+'</h4></button>');
-
         $input.appendTo($("#animal-topics"));	  
 	},
 };
 
 
+// Display one button for each animal topic in topic array
 giphyPortfolio.initButtons();
 
 
 // When page is loaded
 $(document).ready(function() {
 
-	
- //   $(".gif").on("click", function() {
-   $(document).on("click", ".gif", function() { 	
-    	console.log(this);
+	// When Add Animal Submit button is clicked
+	$(document).on("click", "#add-animal", function(event) { 
 
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-      var state = $(this).attr("data-state");
-
-      console.log(state);
-
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    });
-
-	
-	$("#run-search").on("click", function(event) {
-	  	// This line allows us to take advantage of the HTML "submit" property
-	  	// This way we can hit enter on the keyboard and it registers the search
-	  	// (in addition to clicks).
+		// Don't refresh 
 	  	event.preventDefault();
 
-	  	// This line of code will grab the input from the textbox
+	  	// Get entered topic
 		var animal = $('#added-animal').val().trim();
 
-		// Add animal to topic array
-		giphyPortfolio.topic.push(animal.replace(/\b\w/g, l => l.toUpperCase()));
+		// If a topic was eneterd
+		if (animal) {
 
-		// Redisplay buttons
-		giphyPortfolio.initButtons();
+			// Add animal to topic array, capitalizing topic
+			giphyPortfolio.topic.push(animal.replace(/\b\w/g, l => l.toUpperCase()));
 
-		$('#added-animal').val("");
+			// Recreate all topic buttons, including newly entered topic
+			giphyPortfolio.initButtons();
+
+			// Clear topic
+			$('#added-animal').val("");
+		}
 	});
 
+	
+ 	// On image click, animate image or make image still
+   	$(document).on("click", ".image-animal", function() { 	
 
-	// 
+      	// Get image animation state
+      	var state = $(this).attr("data-state");
+
+      	// If state = still, then animate image
+      	if (state === "still") {
+        	$(this).attr("src", $(this).attr("data-animate"));
+        	$(this).attr("data-state", "animate");
+
+		// If state = animate, then make image still
+    	} else {
+        	$(this).attr("src", $(this).attr("data-still"));
+        	$(this).attr("data-state", "still");
+      	} 
+    });
+
+
+	// On topic button click, query GIPHY and popualte 10 images
 	$(document).on("click", ".btn-animal", function() {
-//	$(".btn-animal").on("click", function() {		
-		console.log(this.value);
 
+		// Giphy API call
 	  	$.ajax ({
 	    	url: "https://api.giphy.com/v1/gifs/search?q="+this.value+"&api_key=dc6zaTOxFJmzC&limit=10",
 	    	type: "GET",
 
+	    // On successful API call
 	    success: function(response) {
-	      console.log(response.data);
 
 			// Clear existing images
 			$("#animal-images").empty();
 
+			// Get API data
 			var results = response.data;
 
+			// Loop through API data
 	      	for (var idx=0; idx<response.data.length; idx++) {
 
-		      	// Saving the still and animated images
+		      	// Saving the still and animated image URL's
 		        var animiatedImageUrl = results[idx].images.fixed_height.url;
 		        var stillImageUrl = results[idx].images.fixed_height_still.url;
 
-		    	// Creating a paragraph tag with the result item's rating
-		        var p = $('<p>').text("Rating: " + results[idx].rating);
-
-				// Creating and storing an image tag
+		        // Create next image
 		    	var animalImage = $("<img>");
-
-		        // Setting the animalImage src attribute to imageUrl
 		        animalImage.attr("src", stillImageUrl);
 		        animalImage.attr("alt", this.value+" image");
-
 		        animalImage.attr("data-state", "still");
 		        animalImage.attr("data-animate", animiatedImageUrl);
 		        animalImage.attr("data-still", stillImageUrl);
-		        animalImage.attr("class", "gif");
+		        animalImage.attr("class", "image-animal");;
 
-console.log(animalImage);
+		    	// Create item's rating
+		        var ratingCaption = $("<h4>");
+		        ratingCaption.text("Rating: " + results[idx].rating);
+				ratingCaption.attr("class", "caption text-center");
 
-		        // Prepending the catImage to the images div
-		        $("#animal-images").append(animalImage);
-		       	// $("#animal-images").append(p);
+				// Append image
+		       	$("#animal-images").append(animalImage);	
+
+				// Append rating - Not Working
+				$(".image-animal").text("Rating: "+results[idx].rating);	
 	     	}
 	    }, 
 
@@ -116,5 +120,6 @@ console.log(animalImage);
 	    });
 	});	
 });
+
 
 
